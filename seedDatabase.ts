b343507d4ec2011ad8be4d9a51ccaff40cafbecd
Steven Stavrakis <s.a.stavrakis@gmail.com>
2024-06-12 21:37:16 -0400

    comments

--- seedDatabase.ts
+++ seedDatabase.ts
@@ -9,12 +9,22 @@ However, the data does not quite match the schema of the database.
 Use JS to manipulate the data to match the schema.
 */
 
-// IMPORTANT: USE THIS TO GENERATE UUIDS FOR THE ID FIELD
-// Read the MDN docs for more information
+/*
+IMPORTANT: USE THIS TO GENERATE UUIDS FOR THE ID FIELD
+Read the MDN docs for more information
 
-// EXAMPLE:
-// const id = crypto.randomUUID();
+EXAMPLE:
+const id = crypto.randomUUID();
+*/
 
+/**
+ * This function takes a db instance and seeds the database with the data from the userData JSON file.
+ * 
+ * It is used in the setupDatabase.ts file.
+ * 
+ * @param db The drizzle database instance
+ * @returns {void}
+ */
 export const seedDatabase = async (db: BunSQLiteDatabase<Record<string, never>>) => {
 
     // all the data you need is in the userData import
